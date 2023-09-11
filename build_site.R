dac8294e1695a327d785035ba59af2315a39a1e1
Kim Hochstedler <kimhochstedler@gmail.com>
2023-09-11 10:47:27 -0400

    add all files

---
+++ build_site.R
@@ -0,0 +1,11 @@
+#Set working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/hochsted/OneDrive/Documents/GitHub/teaching_portfolio")
+
+
+#Render your site. 
+rmarkdown::render_site()
+
+
